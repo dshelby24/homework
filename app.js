1d284eae0a4e47f3b67e74635571878c27242bbb
domynique <dshelbyv@gmail.com>
2021-04-14 20:28:17 -0400

    new

---
+++ app.js
@@ -0,0 +1,29 @@
+var a = 2; 
+var b = 4; 
+var d = 6; 
+var e = 8;
+var f = 10;
+var g = 12;
+var h = 14;
+var i = 16;
+var j = 18;
+var k = 20;
+
+//two times addition
+console.log(a+e);
+console.log(d+b);
+//two times subtraction 
+console.log(a-e);
+console.log(d-b);
+//two times multiply 
+console.log(f*i);
+console.log (h*k);
+//two times division 
+console.log(k/f)
+console.log(b/a)
+//two times increments  
+i++
+g++
+//two times decrement 
+f--
+h--
